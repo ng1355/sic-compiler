69e21361beb3d2d84710550bfcdfbe1d7e9de71c
Jack Martinez <jm5833@nyu.edu>
2018-04-15 18:41:50 -0400

    actual cpp file

--- symbol_table_gen.cpp
+++ symbol_table_gen.cpp
@@ -1,50 +1,142 @@
-#include <stack>
-#include <unordered_map>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <iostream>
-#include <regex>
+#include "token_table.hpp" 
 
-struct token{
-    token(const std::string& type, const std::string& name, const int line,
-          const std::string& ret = "", const std::string& param = "",
-          bool defined = false); 
+//--------------------------
+//  token struct definitions
+//--------------------------
 
-    std::string type;
-    std::string name;
-    int line; 
-    /* below members are just for functions */ 
-    std::string ret;
-    std::string param;
-    bool defined;  /* prevents function redefinition */ 
-};
+token::token(const std::string& type, const std::string& name, const int line,
+             const int param, const bool defined):   
+		     type(type), name(name), line(line), param(param),
+             defined(defined) {}
 
+//-------------------------------
+//  token_table class definitions
+//-------------------------------
 
-class token_table{
-public:
-    // type of following variables/ret type for function. useful for 
-    // initialization lists (eg. int a, b, c...) 
-    void decl_type(const char *type);
-    // add either float or int
-    int addvar(const std::string name, const int line_no);
-    // used for function DECLERATIONS 
-    int addfunc(const std::string name, const int line_no, const std::string ret, const std::string param);
-    // used for function DEFINITIONS 
-    int definefunc(const std::string name, const int line_no); 
-    // check if we can call a function by checking if its defined 
-    bool call_func(const std::string name);
-    // check if a variable has been initialized, first in local,
-    // then in global space 
-    bool is_var_init(const std::string name);
-    // if local.empty() is true, we are in global space  
-    void operator ++ (); // enter scope 
-    void operator -- (); // exit scope  
+// set the current type
+void token_table::decl_type(const char *type){
+//    std::cout <<  "declared type: " << type << '\n';
+    current_type = type;
+}
 
-private:
-	//scope, token list
-    std::stack<std::map<std::string, token> > local;
-	//name, token
-    std::map<std::string, token> global;
-    std::string current_type; 
-};
+// add a variable to the token table 
+// returns 0 on sucess, -1 on failure
+void token_table::addvar(const std::string name, const int line_no){
+    // if local is empty, we're in global space.
+	struct token var = token(current_type, name, line_no);
+    if(local.empty()){
+    	if(global.count(name)) return; 
+		global.insert(std::pair<std::string, struct token>(name, var));
+		std::cout << "Global " << current_type << " variable ";
+    } else {
+        if(local.top().count(name)) return;
+		local.top().insert(std::pair<std::string, struct token>(name, var));
+		std::cout << "Local " << current_type << " variable ";
+    }
+	std::cout << name << " declared in line " << line_no << "\n";
+	return;
+}
+
+// add a function declaration 
+// returns 0 if successful, -1 if that function was already declared 
+void token_table::addfunc(const std::string name, const int param, const int line_no, const bool defined){
+    if(global.count(name)) return;
+	struct token func = token(current_type, name, line_no, param, defined);
+	global.insert(std::pair<std::string, struct token>(name, func));
+	if(!defined) std::cout << "Function "  << current_type << ' ' << name << "(" << 
+        ((param) ? "float" : "int") << ") declared in line " << line_no << "\n"; 
+    return;
+}
+
+// add a function definition
+// returns 0 if successful, -1 if its already been declared
+void token_table::definefunc(const std::string name, const int param, const int line_no){
+    if(global.count(name)){
+		std::map<std::string, struct token>::iterator func = global.find(name);
+		if(func->second.defined){
+            std::cout << "Error: Function " << name << " redefinition on line " << 
+                line_no << '\n';
+            return;
+        }
+		func->second.defined = true;
+		std::cout << "Function " << name << " defined in line " << line_no << "\n";
+        return;
+    } else {
+        addfunc(name, param, line_no, true);
+		std::cout << "Function " << current_type << ' ' << name << 
+            '(' << ((param) ? "float" : "int") << ") defined in line " 
+            << line_no << "\n";
+    }
+}
+
+// check if a function is defined 
+void token_table::call_func(const std::string name, const int line_no){
+    auto func = global.find(name);
+    if(func != global.end()){
+        std::cout << "Function " << name << ((func->second.defined) ? " defined" : 
+            " declared") << " in line " << func->second.line <<
+            " used in line " << line_no << '\n';
+    } else {
+        std::cout << "Error: No such function " << name << " on line " <<
+            line_no << '\n';
+    }
+}
+
+//check if a variable has been initialized
+//doesnt work help me GOD 
+void token_table::is_var_init(const std::string name, const int line_no){
+    /* 
+	std::stack<std::map<std::string,token> > copy;
+	while(!local.empty()){
+		copy.push(local.top());
+		local.pop();
+		std::map<std::string, struct token>::iterator var_l = copy.top().find(name);
+		if(var_l  == copy.top().end())
+			continue;
+        std::cout << "Local variable " << name <<
+            "declared in line " << var_l->second.line << " used in line " <<
+            line_no << '\n';
+		return;
+	}
+	//fix the local stack
+	while(!copy.empty()){
+		local.push(copy.top());
+		copy.pop();
+	}
+	std::map<std::string, struct token>::iterator var_g = global.find(name);
+	if(var_g != global.end()){
+        std::cout << "Global variabel " << name << 
+            " declared in line " << var_g->second.line << " used in line " <<
+            line_no << '\n';
+            return;
+    }
+    std::cout << "Error: Variable " << name << " not previously defined.\n";
+    !!!! SECOND ATTEMP !!!!!
+
+    if(!local.empty()){
+        if(local.top().count(name)){
+            std::cout << "Local variable " << name << " declared in line " <<
+                local.top()[name].line << " used in line " << line_no << '\n';
+            return;
+        }
+    } else {
+        if(global.count(name)){
+            std::cout << "Global variable " << name << "declared in line " << 
+                global[name].line << " used in line " << line_no << '\n';
+            return;
+        } else {
+            std::cout << "Error: Variable referenced before assignment\n";
+        }
+    } */
+}
+
+void token_table::operator ++ (){
+	std::map<std::string, struct token> scope;
+	local.push(scope);
+//    std::cout << "Entering scope " << local.size() << '\n';
+}
+void token_table::operator -- (){
+//    std::cout << "Exiting scope " << local.size() << '\n';
+	local.pop();
+//    std::cout << "Returned to scope " << local.size() << '\n';
+}
